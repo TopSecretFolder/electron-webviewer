40622359c02a18ea0b9003df36dd83a74f553732
sattfatt <satyam.patel@rockbot.com>
2024-05-26 18:52:25 -0700

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,77 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import commandLineArgs, { OptionDefinition } from "command-line-args";
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require("electron-squirrel-startup")) {
+  app.quit();
+}
+
+const createWindow = () => {
+  // parse out command line arguments
+  const opts: OptionDefinition[] = [
+    {
+      name: "url",
+      alias: "u",
+      type: String,
+    },
+    {
+      name: "width",
+      alias: "w",
+      type: Number,
+      defaultValue: 800,
+    },
+    {
+      name: "height",
+      alias: "h",
+      type: Number,
+      defaultValue: 600,
+    },
+  ];
+
+  const args = commandLineArgs(opts);
+  console.log("command line arguments:", process.argv);
+  console.log("parsed arguments", args);
+
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: args.width,
+    height: args.height,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  // // and load the index.html of the app.
+  // if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+  //   mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  // } else {
+  //   mainWindow.loadFile(
+  //     path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`),
+  //   );
+  // }
+  mainWindow.loadURL(args.url);
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on("ready", createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  app.quit();
+});
+
+app.on("activate", () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
